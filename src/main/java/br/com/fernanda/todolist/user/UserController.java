9d2d5b2dcbb19c4f26e8e14167f89eecfb7c6507
fwrnandaaa <claudiazkn432@gmail.com>
2025-08-19 18:28:29 -0300

    Entidade Usuário

---
+++ src/main/java/br/com/fernanda/todolist/user/UserController.java
@@ -0,0 +1,25 @@
+package br.com.fernanda.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+/* 
+ * Modificadores
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create( @RequestBody UserModel userModel){
+        System.out.println(userModel.getUsername());
+
+    }
+    
+}
---
+++ src/main/java/br/com/fernanda/todolist/user/UserModel.java
@@ -0,0 +1,27 @@
+package br.com.fernanda.todolist.user;
+
+public class UserModel {
+   private String username;
+   private String name;
+   private String password;
+
+   public void setUsername(String username){
+    this.username = username;
+   }
+   public String getUsername(){
+    return this.username;
+   }
+
+   public String getName() {
+       return name;
+   }
+   public void setName(String name) {
+       this.name = name;
+   }
+   public void setPassword(String password) {
+       this.password = password;
+   }
+   public String getPassword() {
+       return password;
+   }
+}
