3d6194df3cdfc5d9c866151522f9a770ee56db8e
fwrnandaaa <claudiazkn432@gmail.com>
2025-08-19 19:44:06 -0300

    Importando e usando Lombok

--- src/main/java/br/com/fernanda/todolist/user/UserModel.java
+++ src/main/java/br/com/fernanda/todolist/user/UserModel.java
@@ -1,27 +1,11 @@
 package br.com.fernanda.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
    private String username;
    private String name;
    private String password;
 
-   public void setUsername(String username){
-    this.username = username;
-   }
-   public String getUsername(){
-    return this.username;
-   }
-
-   public String getName() {
-       return name;
-   }
-   public void setName(String name) {
-       this.name = name;
-   }
-   public void setPassword(String password) {
-       this.password = password;
-   }
-   public String getPassword() {
-       return password;
-   }
 }
