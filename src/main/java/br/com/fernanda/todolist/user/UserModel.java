a6f50f4e5775d844436f7a45f26d3c4655beb885
fwrnandaaa <claudiazkn432@gmail.com>
2025-08-21 17:45:56 -0300

    configuração do banco de dados

--- src/main/java/br/com/fernanda/todolist/user/UserModel.java
+++ src/main/java/br/com/fernanda/todolist/user/UserModel.java
@@ -1,11 +1,20 @@
 package br.com.fernanda.todolist.user;
-
+import java.time.LocalDateTime;
+import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+   @Id
+   @GeneratedValue(generator = "UUID")
+   private UUID id;
    private String username;
    private String name;
    private String password;
 
+   private LocalDateTime createdAt;
 }
